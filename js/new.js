713519e41caf2cb7594bc6ad9fa1955abf7ec90c
sophia0110 <njjp6ru@yahoo.com.tw>
2022-05-26 23:15:17 +0800

    Update new.js

--- js/new.js
+++ js/new.js
@@ -5,8 +5,8 @@ $("#tab li").on("click", function () {
 // 2021/12/17 修改
 var innerHtml =
 '<div class="item" >' +
-'<div class="card" onclick="">' +
-'<img class="cardImg" src="../images/翻牌遊戲.gif" alt="">' +
+'<div class="card" >' +
+'<img class="cardImg" src="Portfolio/images/翻牌遊戲.gif" alt="">' +
 '<div class="cardtitle">翻牌配對遊戲</div>' +
 '<div class="cardContent">' +
 "<p>運用click 改變class的樣式，呈現配對正確出現恭喜!恭喜提示，配對錯誤出現再試試吧。</p>" +
@@ -15,7 +15,7 @@ var innerHtml =
 "</div>"+
   '<div class="item">' +
   '<div class="card">' +
-  '<img class="cardImg" src="../images/手風琴動畫.gif" alt="">' +
+  '<img class="cardImg" src="Portfolio/images/手風琴動畫.gif" alt="">' +
   '<div class="cardtitle">手風琴動畫</div>' +
   '<div class="cardContent">' +
   "<p>用運click 改變class的樣式.active 及hover 。</p>" +
@@ -24,7 +24,7 @@ var innerHtml =
   "</div>"+
   '<div class="item">' +
   '<div class="card">' +
-  '<img class="cardImg" src="../images/我的調色盤.gif" alt="">' +
+  '<img class="cardImg" src="Portfolio/images/我的調色盤.gif" alt="">' +
   '<div class="cardtitle">我的調色盤</div>' +
   '<div class="cardContent">' +
   "<p>用運click 改變class的樣式，感應所選顏色。</p>" +
@@ -33,7 +33,7 @@ var innerHtml =
   "</div>"+
   '<div class="item">' +
   '<div class="card">' +
-  '<img class="cardImg" src="../images/購物車價格加總.gif" alt="">' +
+  '<img class="cardImg" src="Portfolio/images/購物車價格加總.gif" alt="">' +
   '<div class="cardtitle">購物車價格加總及視窗進度條</div>' +
   '<div class="cardContent">' +
   "<p>用運scrollTop，取得目前在視窗的位置，運用keyup及find 取得水果價格，並進行加總。</p>" +
@@ -45,7 +45,7 @@ $("#jqueryCarousel")[0].innerHTML = innerHtml;
 var rwdinnerHtml =
 '<div class="item">' +
 '<div class="card">' +
-'<img class="cardImg" src="../images/美髮網站切版.gif" alt="">' +
+'<img class="cardImg" src="Portfolio/images/美髮網站切版.gif" alt="">' +
 '<div class="cardtitle">美髮網站切版</div>' +
 '<div class="cardContent">' +
 "<p>運用HTML及CSS，進行RWD切版</p>" +
@@ -54,7 +54,7 @@ var rwdinnerHtml =
 "</div>"+
   '<div class="item">' +
   '<div class="card">' +
-  '<img class="cardImg" src="../images/Bootstrap.gif" alt="">' +
+  '<img class="cardImg" src="Portfolio/images/Bootstrap.gif" alt="">' +
   '<div class="cardtitle">網站切版</div>' +
   '<div class="cardContent">' +
   "<p>運用bootstrap，進行切版。</p>" +
@@ -64,28 +64,28 @@ var rwdinnerHtml =
 $("#RWDCarousel")[0].innerHTML = rwdinnerHtml;
 var JQueryListHtml=
 '<div class="listCard row">'+
-'<img class="listCardImg" src="images/翻牌遊戲.gif" alt="">' +
+'<img class="listCardImg" src="Portfolio/images/翻牌遊戲.gif" alt="">' +
 '<div class="listCardContent">'+
     '<div class="listCardtitle">翻牌配對遊戲</div>'+
    '<p>運用click 改變class的樣式，呈現配對正確出現恭喜!恭喜提示，配對錯誤出現再試試吧。</p>' +
 '</div>'+
 '</div>'+
 '<div class="listCard row">'+
-'<img class="listCardImg" src="images/手風琴動畫.gif" alt="">' +
+'<img class="listCardImg" src="Portfolio/images/手風琴動畫.gif" alt="">' +
 '<div class="listCardContent">'+
     '<div class="listCardtitle">手風琴動畫</div>'+
    '<p>用運click 改變class的樣式.active 及hover 。</p>' +
 '</div>'+
 '</div>'+
 '<div class="listCard row">'+
-'<img class="listCardImg" src="images/我的調色盤.gif" alt="">' +
+'<img class="listCardImg" src="Portfolio/images/我的調色盤.gif" alt="">' +
 '<div class="listCardContent">'+
     '<div class="listCardtitle">我的調色盤</div>'+
    '<p>用運click 改變class的樣式，感應所選顏色。</p>' +
 '</div>'+
 '</div>'+
 '<div class="listCard row">'+
-'<img class="listCardImg" src="images/購物車價格加總.gif" alt="">' +
+'<img class="listCardImg" src="Portfolio/images/購物車價格加總.gif" alt="">' +
 '<div class="listCardContent">'+
     '<div class="listCardtitle">購物車價格加總及視窗進度條</div>'+
    '<p>用運scrollTop，取得目前在視窗的位置，運用keyup及find 取得水果價格，並進行加總。</p>' +
@@ -96,14 +96,14 @@ $("#JQuerytotalList")[0].innerHTML = JQueryListHtml;
 
 var RwdListHtml=
 '<div class="listCard row">'+
-'<img class="listCardImg" src="images/美髮網站切版.gif" alt="">' +
+'<img class="listCardImg" src="Portfolio/images/美髮網站切版.gif" alt="">' +
 '<div class="listCardContent">'+
     '<div class="listCardtitle">美髮網站切版</div>'+
    '<p>運用HTML及CSS，進行RWD切版。</p>' +
 '</div>'+
 '</div>'+
 '<div class="listCard row">'+
-'<img class="listCardImg" src="images/Bootstrap.gif" alt="">' +
+'<img class="listCardImg" src="Portfolio/images/Bootstrap.gif" alt="">' +
 '<div class="listCardContent">'+
     '<div class="listCardtitle">網站切版</div>'+
    '<p>運用bootstrap，進行切版。</p>' +
